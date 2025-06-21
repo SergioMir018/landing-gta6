2a0c36498bb3348457db66a4ce3ff833d28889f2
SergioMir018 <sergiommirs@gmail.com>
2025-06-21 13:13:03 -0400

    added github deploy action

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://sergiomir018.github.io',
+  base: '/landing-gta6',
   vite: {
     plugins: [tailwindcss()]
   }
